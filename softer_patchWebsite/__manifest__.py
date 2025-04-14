83f373ab26215d0fda0e8f74f085d80c8c8efb03
alejandro <alejandro@softer.com.ar>
2025-04-14 09:22:24 -0300

    patch website

--- softer_patchWebsite/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Website Request URI Patch",
-    "version": "1.0",
-    "summary": "Evita errores con REQUEST_URI al renderizar el layout",
-    "author": "Fix by ChatGPT 😎",
-    "depends": ["website"],
-    "data": [],
-    "installable": True,
-}
